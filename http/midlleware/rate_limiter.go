64042c76e5eb3bc61fd284c752bdfd3f53b7834a
pramudya3 <pramudya500@gmail.com>
2024-02-12 06:52:01 +0700

    feat: add middleware

---
+++ http/midlleware/rate_limiter.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+)
+
+func RateLimiter() gin.HandlerFunc {
+	limiter := rate.NewLimiter(1, 5)
+	return func(c *gin.Context) {
+		if limiter.Allow() {
+			c.Next()
+		} else {
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"message": "Limit exceed",
+			})
+		}
+	}
+}
